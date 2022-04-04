aa13317bd011891b7d068c35ac591d96aac488a7
Casico Dmitrii <kasikods2014@gmail.com>
2022-04-04 18:37:35 +0300

    Lab4New

--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -1,8 +1,63 @@
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Form, Input, InputNumber, Button, message } from 'antd';
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: 'This is not a valid email!',
+        number: '${label} is not a valid number!',
+        url: 'This is not a valid url!'
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const Demo = () => {
+    const onFinish = (values: any) => {
+        console.log('Success', values);
+        message.success('Info was sent')
+    };
+
+    return (
+        <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+            <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email', required: true }]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 18, max: 99 }]}>
+                <InputNumber />
+            </Form.Item>
+            <Form.Item name={['user', 'website']} label="Website" rules={[{type:'url'}]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name={['user', 'introduction']} label="Introduction">
+                <Input.TextArea />
+            </Form.Item>
+            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                <Button type='primary' htmlType="reset">
+                    Reset
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
 
 export const LayoutCustom = () => {
     return(
@@ -48,7 +103,9 @@ export const LayoutCustom = () => {
                                 </SubMenu>
                             </Menu>
                         </Sider>
-                        <Content style={{ padding: '0 24px', minHeight: 280 }}>Casico Dmitrii RM-192</Content>
+                        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                            <Demo />
+                        </Content>
                     </Layout>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
